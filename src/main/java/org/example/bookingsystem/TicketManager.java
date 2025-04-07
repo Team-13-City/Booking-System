6c9772b4b6b72f8459d0b0d7b5f9a2898131424b
jenson-bolton-ac <115472244+jenson-bolton-ac@users.noreply.github.com>
2025-04-07 16:02:33 +0100

    changed user to customer

--- src/main/java/org/example/bookingsystem/TicketManager.java
+++ src/main/java/org/example/bookingsystem/TicketManager.java
@@ -1,20 +1,26 @@
 package org.example.bookingsystem;
 
+import java.util.List;
+
 public class TicketManager {
     List<Ticket> tickets;
 
-    Ticket sellTicket(User user, Event event, Seat seat, Discount discount) {
+    Ticket sellTicket(Customer customer, Event event, Seat seat, Discount discount) {
 
     }
+
     boolean cancelTicket(String ticketID) {
 
     }
+
     boolean refundTicket(String ticketID) {
 
     }
+
     List<Ticket> getTicketsByUser(User user) {
 
     }
+
     List<Ticket> getTicketsByEvent(Event event) {
 
     }
