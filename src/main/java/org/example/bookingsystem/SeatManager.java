e65e04453b2ff53491dad6671abe6db4b4f383e2
jenson-bolton-ac <115472244+jenson-bolton-ac@users.noreply.github.com>
2025-04-07 17:06:56 +0100

    Update SeatManager.java

--- src/main/java/org/example/bookingsystem/SeatManager.java
+++ src/main/java/org/example/bookingsystem/SeatManager.java
@@ -1,4 +1,16 @@
 package org.example.bookingsystem;
 
+import java.util.List;
+import java.util.Map;
+
 public class SeatManager {
+    Map<Integer, List<Seat>> eventSeatMap; // eventID -> seats
+
+    boolean reserveSeat (Event event, int seatID) {
+
+    }
+
+    boolean releaseSeat (Event event, int seatID) {
+
+    }
 }
