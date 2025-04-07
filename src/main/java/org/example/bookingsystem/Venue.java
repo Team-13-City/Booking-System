0d1708e7164a2b5127a5776f39dfee50c8a2f640
jenson-bolton-ac <115472244+jenson-bolton-ac@users.noreply.github.com>
2025-04-07 16:33:06 +0100

    Create Venue.java

---
+++ src/main/java/org/example/bookingsystem/Venue.java
@@ -0,0 +1,22 @@
+package org.example.bookingsystem;
+
+import java.util.Map;
+
+public class Venue {
+    String venueID;
+    String name;
+    int capacity;
+    Map<String, Seat> layout; // seatID -> Seat
+
+    Seat getSeat(String seatID) {
+        return layout.get(seatID);
+    }
+
+    List<Seat> getAllSeats() {
+
+    }
+
+    List<Seat> getAccessibleSeats() {
+
+    }
+}
